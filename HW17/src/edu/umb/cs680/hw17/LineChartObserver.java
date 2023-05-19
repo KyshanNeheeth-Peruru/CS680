5c8205e92a1b3e7cfd20faaa0c57b0533f82672f
KyshanNeheeth-Peruru <44707101+KyshanNeheeth-Peruru@users.noreply.github.com>
2023-05-19 13:13:04 -0400

    Update LineChartObserver.java

--- HW17/src/edu/umb/cs680/hw17/LineChartObserver.java
+++ HW17/src/edu/umb/cs680/hw17/LineChartObserver.java
@@ -1,12 +1,11 @@
 package edu.umb.cs680.hw17;
 
-public class LineChartObserver implements Observer {
+public class LineChartObserver implements Observer<StockEvent> {
 
-	public void update(Observable sender, Object event) {
+	public void update(Observable<StockEvent> sender, StockEvent event) {
 		StockEvent se = (StockEvent) event;
 		System.out.println("LineChart Observer:");
 		System.out.println("Ticker:"+se.getTicker());
-		System.out.println("Quote:"+se.getQuote());
+		System.out.println("Quote:"+se.getQuote());	
 	}
-
 }
