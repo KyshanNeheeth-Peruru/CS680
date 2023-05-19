c0fd54dd95fadcfbf86ba6bde83b0b2c4f410a7c
KyshanNeheeth-Peruru <44707101+KyshanNeheeth-Peruru@users.noreply.github.com>
2023-05-19 13:14:24 -0400

    Update Observer3D.java

--- HW17/src/edu/umb/cs680/hw17/Observer3D.java
+++ HW17/src/edu/umb/cs680/hw17/Observer3D.java
@@ -1,10 +1,10 @@
 package edu.umb.cs680.hw17;
 
-public class Observer3D implements Observer {
-	public void update(Observable sender, Object event) {
+public class Observer3D implements Observer<StockEvent> {
+	public void update(Observable<StockEvent> sender, StockEvent event) {
 		StockEvent se = (StockEvent) event;
 		System.out.println("3D Observer:");
-		System.out.println("Quote:"+se.getQuote());
-		System.out.println("Ticker:"+se.getTicker());		
+		System.out.println("Ticker:"+se.getTicker());
+		System.out.println("Quote:"+se.getQuote());	
 	}
 }
