844d12eefae1f45fbd6ba48758c7e574fa8162fa
KyshanNeheeth-Peruru <44707101+KyshanNeheeth-Peruru@users.noreply.github.com>
2023-05-19 13:15:35 -0400

    Update TableObserver.java

--- HW17/src/edu/umb/cs680/hw17/TableObserver.java
+++ HW17/src/edu/umb/cs680/hw17/TableObserver.java
@@ -1,12 +1,11 @@
 package edu.umb.cs680.hw17;
 
-public class TableObserver implements Observer {
+public class TableObserver implements Observer<StockEvent> {
 
-	public void update(Observable sender, Object event) {
+	public void update(Observable<StockEvent> sender, StockEvent event) {
 		StockEvent se = (StockEvent) event;
-		System.out.println("Table Oberver:");
+		System.out.println("Table Observer:");
 		System.out.println("Ticker:"+se.getTicker());
-		System.out.println("Quote:"+se.getQuote());
+		System.out.println("Quote:"+se.getQuote());	
 	}
-
 }
