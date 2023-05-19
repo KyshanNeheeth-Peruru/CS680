ab969ce518cb4b1e9451a9f12911abe6d961ec8a
KyshanNeheeth-Peruru <44707101+KyshanNeheeth-Peruru@users.noreply.github.com>
2023-05-19 13:15:19 -0400

    Update StockQuoteObservable.java

--- HW17/src/edu/umb/cs680/hw17/StockQuoteObservable.java
+++ HW17/src/edu/umb/cs680/hw17/StockQuoteObservable.java
@@ -3,11 +3,11 @@ package edu.umb.cs680.hw17;
 import java.util.HashMap;
 import java.util.Map;
 
-public class StockQuoteObservable extends Observable {
+public class StockQuoteObservable extends Observable<StockEvent> {
+	
 	Map<String, Double> tq = new HashMap<String,Double>();
 	public void changeQuote(String t, double q) {
 		tq.put(t, q);
 		notifyObservers(new StockEvent(t,q));
 	}
-
 }
